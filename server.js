209b9031a4c646b5f82bdb887bdaac4d34794006
UzomaAkpala <francisakpala@yahoo.com>
2023-11-03 15:05:45 +0100

    Prepare for Creating API

--- server.js
+++ server.js
@@ -4,18 +4,17 @@ const app = express();
 
 const port = 3000;
 
-app.use((req, res, next) => {
-  console.log("first middle ware");
-  next();
-});
+const apiRoutes = require("./routes/apiRoutes");
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.json({ message: "Api running" });
 });
 
-app.get("/two", (req, res) => {
-  res.send("Hello World 2!");
-});
+app.use("/api", apiRoutes);
+
+// app.get("/api/products", (req, res) => {
+//   res.send("Handling product routes.");
+// });
 
 app.listen(port, () => {
   console.log("Example listening on port 3000");
