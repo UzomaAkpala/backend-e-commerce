1e14c250d5b91ad3d465516728f6da15811de104
UzomaAkpala <francisakpala@yahoo.com>
2023-11-03 14:46:02 +0100

    middleware

--- server.js
+++ server.js
@@ -4,8 +4,17 @@ const app = express();
 
 const port = 3000;
 
+app.use((req, res, next) => {
+  console.log("first middle ware");
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/two", (req, res) => {
+  res.send("Hello World 2!");
 });
 
 app.listen(port, () => {
