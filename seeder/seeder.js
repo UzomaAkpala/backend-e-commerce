b5110e4b3473cec65fc3fda5accf401719c3b245
UzomaAkpala <francisakpala@yahoo.com>
2023-11-06 15:55:28 +0100

    seeder for product

--- seeder/seeder.js
+++ seeder/seeder.js
@@ -1,19 +1,29 @@
-const connectDB = require("../config/db");
-connectDB();
+const connectDB = require("../config/db")
+connectDB()
 
-const categoryData = require("./categories");
-const Category = require("../models/CategoryModel");
+const categoryData = require("./categories")
+const productData = require("./products")
+
+const Category = require("../models/CategoryModel")
+const Product = require("../models/ProductModel")
 
 const importData = async () => {
-  try {
-    await Category.collection.dropIndexes();
-    await Category.collection.deleteMany({});
-    await Category.insertMany(categoryData);
-    console.log("Seeder data proceeded successfully");
-    process.exit();
-  } catch (error) {
-    console.error("Error while processing seeder data", error);
-    process.exit(1);
-  }
-};
-importData();
+    try {
+        await Category.collection.dropIndexes()
+        await Product.collection.dropIndexes()
+
+        await Category.collection.deleteMany({})
+        await Product.collection.deleteMany({})
+
+        await Category.insertMany(categoryData)
+        await Product.insertMany(productData)
+
+        console.log("Seeder data proceeded successfully")
+        process.exit()
+    } catch (error) {
+        console.error("Error while proccessing seeder data", error)
+        process.exit(1);
+    }
+}
+importData()
+ 
