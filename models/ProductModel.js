e843426d118d3424f9c90b4a8f7b5e4b7d26d52c
UzomaAkpala <francisakpala@yahoo.com>
2023-11-06 12:29:06 +0100

    Finish Product Model

--- models/ProductModel.js
+++ models/ProductModel.js
@@ -1,10 +1,48 @@
 const mongoose = require("mongoose");
 
-const productSchema = mongoose.Schema({
-  name: {
-    type: String,
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    count: {
+      type: Number,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    rating: {
+      type: Number,
+    },
+    reviewsNumber: {
+      type: Number,
+    },
+    sales: {
+      type: Number,
+      default: 0,
+    },
+    attrs: [{ key: { type: String }, value: { type: String } }],
+    images: [],
+    reviews: [],
   },
-});
+  {
+    timestamps: true,
+  }
+);
+
+productSchema.index();
 
 const Product = mongoose.model("Product", productSchema);
 
--- server.js
+++ server.js
@@ -7,20 +7,7 @@ const port = 3000;
 const apiRoutes = require("./routes/apiRoutes");
 
 app.get("/", async (req, res, next) => {
-  const Product = require("./models/ProductModel");
-  try {
-    const product = new Product();
-    product.name = "New product name";
-
-    const productSaved = await product.save();
-    console.log(productSaved === product);
-    const products = await Product.find();
-    console.log(products.length);
-    res.send("Product created" + product._id);
-  } catch (error) {
-    next(error);
-  }
-  //res.json({ message: "Api running" });
+  res.json({ message: "Api running" });
 });
 
 // mongodb connection
