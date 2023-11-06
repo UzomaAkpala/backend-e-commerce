690b7b901ecda0c5f45dbb36ba6c9e446f5369d6
UzomaAkpala <francisakpala@yahoo.com>
2023-11-06 12:07:20 +0100

    MongoDb database operation

--- server.js
+++ server.js
@@ -6,13 +6,27 @@ const port = 3000;
 
 const apiRoutes = require("./routes/apiRoutes");
 
-app.get("/", (req, res) => {
-  res.json({ message: "Api running" });
+app.get("/", async (req, res, next) => {
+  const Product = require("./models/ProductModel");
+  try {
+    const product = new Product();
+    product.name = "New product name";
+
+    const productSaved = await product.save();
+    console.log(productSaved === product);
+    const products = await Product.find();
+    console.log(products.length);
+    res.send("Product created" + product._id);
+  } catch (error) {
+    next(error);
+  }
+  //res.json({ message: "Api running" });
 });
 
 // mongodb connection
 
 const connectDb = require("./config/db");
+const Product = require("./models/ProductModel");
 connectDb();
 
 app.use("/api", apiRoutes);
